8108200276b90153b53f38fcc4118c4871f00a67
Terry Li <terryseanli@gmail.com>
2023-05-03 21:38:58 -0700

    filled out homepage elements

--- src/main/Homepage.js
+++ src/main/Homepage.js
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import UserContext from "../auth/UserContext";
 
-return (
-  <main className="TodoApp">
-    <div className="row">
-      <div className="col-md-6">
-        <h3 className="mb-3">Todos</h3>
-        {todos.length > 0
-            ? <EditableTodoList
-                todos={todos}
-                update={update}
-                remove={remove} />
-            : <span className="text-muted">You have no todos.</span>}
-      </div>
+/**
+ * Homepage for Friender site
+ *
+ * Props:
+ *
+ * State:
+ */
 
-      <div className="col-md-6">
-        {todos.length > 0 && (
-            <section className="mb-4">
-              <h3 className="mb-3">Top Todo</h3>
-              <TopTodo todos={todos} />
-            </section>
+function Homepage() {
+  const { currentUser } = useContext(UserContext);
+  console.debug("Homepage", "currentUser=", currentUser);
+
+
+  return (
+    <main className="Homepage">
+      {currentUser
+        ? <div className="row">
+          <div className="col-md-6">
+            <h3 className="mb-3">My Profile</h3>
+            <br />
+            <ul>
+              <li><b>Username: </b>{currentUser.username}</li>
+              <li><b>Email: </b>{currentUser.email}</li>
+              <li><b>Bio: </b>{currentUser.bio}</li>
+              <li><b>Location: </b>{currentUser.location}</li>
+              <li><b>Friend Radius: </b>{currentUser.friendRadius}</li>
+            </ul>
+          </div>
+
+          <div className="col-md-6">
+            <h3 className="mb-3">Friends</h3>
+            {friends.length > 0
+              ? <FriendList friends={friends} />
+              : <span className="text-muted">You have no friends (yet!).</span>}
+          </div>
+        </div>
+        : (
+          <p>
+            <Link className="btn btn-primary fw-bold me-3"
+              to="/login">
+              Log in
+            </Link>
+            <Link className="btn btn-primary fw-bold"
+              to="/signup">
+              Sign up
+            </Link>
+          </p>
         )}
+    </main>
+  );
+}
 
-        <section>
-          <h3 className="mb-3">Add Nü</h3>
-          <TodoForm handleSave={create} />
-        </section>
-      </div>
-
-    </div>
-  </main>
-);
+export default Homepage;
